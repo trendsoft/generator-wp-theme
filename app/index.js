2f1cf4a2daea10154636ee36c86760993fb54250
jabber <2898117012@qq.com>
2017-05-03 23:50:19 +0800

    init

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
 'use strict';
 var generators = require('yeoman-generator');
+module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+        this.option('coffee');
+        console.log('coffee');
+    },
+    method1: function () {
+        console.log('method 1 just ran');
+    },
+    method2: function () {
+        console.log('method 2 just ran');
+    }
+});
